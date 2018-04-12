433870d41774ce294bee830d25bdbc57b6d7655e
Isis Tejeda <isis@isistejeda.com>
2018-04-12 12:55:42 -0500

    adding sidebar

--- components/houses.ctr.js
+++ components/houses.ctr.js
@@ -2,13 +2,19 @@
     "use strict";
     angular
         .module("ngHouses")
-        .controller("housesCtrl", function($scope, $http, housesFactory) {
+        .controller("housesCtrl", function($scope, $http, housesFactory, $mdSidenav) {
            
            housesFactory.getHouses().then(function(houses){
             $scope.houses = houses.data;
             // console.log(data);
-           })   // if over server, you put the url here
+           });   // if over server, you put the url here
 
+           $scope.openSidebar = function(){
+                $mdSidenav('left').open();
+           }
+            $scope.closeSidebar = function(){
+                $mdSidenav('left').close();
+           }
            
         });
 }) ();
