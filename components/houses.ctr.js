39dcdd74a9ba973b24386a017b3e375096e6a4fd
Isis Harris <isis.harris@ipeople.org>
2018-04-13 14:04:08 -0500

    working on deleting item

--- components/houses.ctr.js
+++ components/houses.ctr.js
@@ -2,12 +2,18 @@
     "use strict";
     angular
         .module("ngHouses")
-        .controller("housesCtrl", function($scope, $http, housesFactory, $mdSidenav) {
+        .controller("housesCtrl", function($scope, $http, housesFactory, $mdSidenav, $mdToast, $mdDialog ) {
            
            housesFactory.getHouses().then(function(houses){
             $scope.houses = houses.data;
             // console.log(data);
            });   // if over server, you put the url here
+
+           // Place holder for fake data profile
+        //    var personSeller = { 
+        //        name: 'mimis'
+               
+        //    }
 
            $scope.openSidebar = function(){
                 $mdSidenav('left').open();
@@ -18,7 +24,54 @@
            
 
             $scope.saveHouse = function(house){
+                if(house){
+                // house.personSeller = contact;   //render part of profile
                 $scope.houses.push(house);
+                $scope.classified = {} ;
+                $scope.closeSidebar();   
+                showToast('house saved');
             }
+
+            $scope.editHouse = function(house){
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.house = house;
+            }
+
+            $scope.saveEdit = function(){
+                $scope.editing = false;
+                $scope.classified = {} ;
+                $scope.closeSidebar();
+                showToast('edit saved');
+            }
+
+            $scope.deleteHouse = function(event, house){
+                var confirm = $mdDialog.confirm()
+                // .title(`are you sure you want to delte {{house.title}}`)
+                    .title('are you sure you want to delte ' + house.title)
+                    .ok('yes')
+                    .cancel('no')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(funtion() {
+                    
+                })
+
+                var index = $scope.houses.indexOf(house);
+                if(confirm){
+
+                $scope.houses.splice(index,1);
+                }
+            }
+
+            function showToast(message) {
+                $mdToast.show(  
+                    $mdToast.simple()                  
+                    .content(message)
+                    .position('top right')
+                    .hideDelay(2000)
+                    );
+                };
+            }
+
         });
 }) ();
