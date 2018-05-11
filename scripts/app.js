201ed65c204b6b503c6374b5b6c505e8258be5cb
Isis Tejeda <isis@isistejeda.com>
2018-05-11 08:14:41 -0500

    resuming...

--- scripts/app.js
+++ scripts/app.js
@@ -8,24 +8,24 @@ angular.module("ngHouses", ["ngMaterial", "ui.router"])
         url: '/stateone',
         template: '<h1>{{ stateone.message }}</h1>',
         controller: 'stateOneCtrl as stateone'
-      })
-        .state( 'statetwo', {
-        url: '/statetwo',
-        template: 'Me 2 <div> <md-button ui-sref="two.more"> go to nested </md-button><ui-view></ui-view> </div>'
-      })
-       .state('statetwo.more', {
-        url: '/more',  
-        template: ' I am in the inside/deep end '
+      });
+      //   .state( 'statetwo', {
+      //   url: '/statetwo',
+      //   template: 'Me 2 <div> <md-button ui-sref="two.more"> go to nested </md-button><ui-view></ui-view> </div>'
+      // })
+      //  .state('statetwo.more', {
+      //   url: '/more',  
+      //   template: ' I am in the inside/deep end '
 
-       });
+      //  });
 
-  })
+  });
 
 
-.controller('stateOneCtrl', function($scope){
-  var vm = this;
-  vm.message = "im from state one";
-});
+// .controller('stateOneCtrl', function($scope){
+//   var vm = this;
+//   vm.message = "im from state one";
+// });
   
   // the line 15 is not renedering as a nested state
   
